a99c1c3471df90298a5463ca4befe1412d14637b
devi <devi.sambandhan@treselle.com>
2017-06-27 16:55:17 +0530

    Added lod for source files

--- src/DiscernDemo.java
+++ src/DiscernDemo.java
@@ -1,5 +1,5 @@
 public class DiscernDemo {
 	public static void main(String args[]) {
-		System.out.println("Hellooo!!! This is my first Discern POC!!");
+		System.out.println("Hellooo!!! This is my first Discern POC!!!");
 	}
 }
